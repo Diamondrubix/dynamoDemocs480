aa766ced21e38654a158be35af6e3835211d2814
Diamondrubix <Diamondrubix@gmail.com>
2019-04-28 17:07:48 -0500

    testingquerydfiferences

--- cs480Dynamo/dynamoDemoLogin.js
+++ cs480Dynamo/dynamoDemoLogin.js
@@ -3,12 +3,13 @@ const ddb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = function (event, context, callback) {
 
+/*
     ddb.get({
         TableName: 'users',
         Key: { 'username': event.username }
     }).promise().then(function (data) {
-        if (event.password == data.password) {
-            callback(null, { "message": data.authtoken });
+        if (event.password == data.Item.password) {
+            callback(null, { "message": data.Item.authtoken });
         } else {
             callback(null, { "message": "bad password" + JSON.stringify(data) });
         }
@@ -16,7 +17,27 @@ exports.handler = function (event, context, callback) {
 
         callback(null, { "message": "some weird error: " + err });
     });
+    */
 
+
+/*
+    ddb.query({
+        TableName: 'users'
+    }).promise().then(function (data) {
+        
+            callback(null, { "message": JSON.stringify(data)});
+    }).catch(function (err) {
+        //handle error
+    });
+*/
+
+    ddb.scan({
+        TableName: 'users'
+    }).promise().then(function (data) {
+         callback(null, { "message": JSON.stringify(data)});
+    }).catch(function (err) {
+         callback(null, { "message": err});
+    });
 
 
 
