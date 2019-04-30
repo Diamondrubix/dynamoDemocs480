804dda2f5f91395213dedeaa01c81d082f384838
Diamondrubix <Diamondrubix@gmail.com>
2019-04-30 15:32:31 -0500

    addWorks

--- cs480Dynamo/addSongs.js
+++ cs480Dynamo/addSongs.js
@@ -4,38 +4,26 @@ const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
 
     
-    ddb.update({
-        TableName: 'MusicUsers',
-        ReturnValues: 'ALL_NEW',
-        Key: { 'Username': event.username, 'AuthToken': event.authtoken }
-        , ExpressionAttributeValues: { ':songs': 'test' }
-    }).promise().then(function (data) {
-        //your logic goes here
-    }).catch(function (err) {
-        //handle error
-    });
-
-function appendMarkedLocation (personId, location) {
-  return DB.update({
-    TableName: 'people',
-    Key: { id: personId },
+ ddb.update({
+    TableName: 'MusicUsers',
+     Key: { 'Username': event.username, 'AuthToken': event.authtoken },
     ReturnValues: 'ALL_NEW',
     UpdateExpression: 'set #songs = list_append(if_not_exists(#songs, :empty_list), :location)',
     ExpressionAttributeNames: {
       '#songs': 'songs'
     },
     ExpressionAttributeValues: {
-      ':location': [location],
+      ':location': [event.song],
       ':empty_list': []
     }
   }).promise().then(function (data){
       callback(null, { "message": "Successfully executed" });
   }).catch(function(err){
-      callback(null, { "message": "err: "+err });
+      callback(null, { "message": "err: "+err});
   })
-}
 
-appendMarkedLocation(event.username, event.song).then(console.log)
+
+
 
 
 }
